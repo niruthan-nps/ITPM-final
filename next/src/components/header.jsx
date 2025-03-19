6c99b077bc60c54af6fa858ef6b7f6ac75713347
Afaaf Hariri <haririafaaf@gmail.com>
2025-03-19 17:47:11 +0530

    header icons

--- next/src/components/header.jsx
+++ next/src/components/header.jsx
@@ -2,14 +2,18 @@ import { User } from "lucide-react";
 import { LayoutDashboard } from "lucide-react";
 import { NotebookPen } from "lucide-react";
 import { BookOpenCheck } from "lucide-react";
+import { Brain } from "lucide-react";
 
 function Header() {
   return (
     <header className="bg-[var(--background)] text-[var(--foreground)] flex justify-between items-center px-8 py-5 border-b border-[var(--border)]">
-      <div className="shrink-0">
-        <h1 className="text-2xl font-bold">Mental Health Tracker</h1>
+      <div className="flex items-center">
+        <span className="text-3xl font-bold">MindMate</span>
+        <span className="ml-1">
+          <Brain size={28} />
+        </span>
       </div>
-      <nav className="flex space-x-5 list-none text-[var(--foreground)] font-semibold">
+      <nav className="flex space-x-8 list-none font-semibold">
         <li className="hover:text-gray-600 transition-all duration-300">
           <div className="flex items-center">
             <span>Dashboard</span>
@@ -35,7 +39,12 @@ function Header() {
           </div>
         </li>
         <li className="hover:text-gray-600 transition-all duration-300">
-          <User size={24} />
+          <div className="flex items-center">
+            <span>Profile</span>
+            <span className="ml-1">
+              <User size={20} />
+            </span>
+          </div>
         </li>
       </nav>
     </header>
