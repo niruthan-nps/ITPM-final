97344d1b2d01ebad74630e5e34063de919471552
Afaaf Hariri <haririafaaf@gmail.com>
2025-03-21 17:05:20 +0530

    nav-bar

--- next/src/components/nav-bar.jsx
+++ next/src/components/nav-bar.jsx
@@ -5,39 +5,41 @@ import { BookOpenCheck } from "lucide-react";
 
 function NavBar() {
   return (
-    <nav className="flex flex-col space-x-8 list-none font-semibold">
-      <li className="hover:text-gray-600 transition-all duration-300">
-        <div className="flex items-center">
-          <span>Dashboard</span>
-          <span className="ml-1">
-            <LayoutDashboard size={20} />
-          </span>
-        </div>
-      </li>
-      <li className="hover:text-gray-600 transition-all duration-300">
-        <div className="flex items-center">
-          <span>Journals</span>
-          <span className="ml-1">
-            <NotebookPen size={20} />
-          </span>
-        </div>
-      </li>
-      <li className="hover:text-gray-600 transition-all duration-300">
-        <div className="flex items-center">
-          <span>Blogs</span>
-          <span className="ml-1">
-            <BookOpenCheck size={20} />
-          </span>
-        </div>
-      </li>
-      <li className="hover:text-gray-600 transition-all duration-300">
-        <div className="flex items-center">
-          <span>Profile</span>
-          <span className="ml-1">
-            <User size={20} />
-          </span>
-        </div>
-      </li>
+    <nav className="bg-bg-[var(--background)] h-screen w-64 p-6 fixed ">
+      <ul className="flex flex-col space-y-4 list-none font-semibold">
+        <li className="hover:text-gray-600 transition-all duration-300">
+          <div className="flex items-center">
+            <span>Dashboard</span>
+            <span className="ml-1">
+              <LayoutDashboard size={20} />
+            </span>
+          </div>
+        </li>
+        <li className="hover:text-gray-600 transition-all duration-300">
+          <div className="flex items-center">
+            <span>Journals</span>
+            <span className="ml-1">
+              <NotebookPen size={20} />
+            </span>
+          </div>
+        </li>
+        <li className="hover:text-gray-600 transition-all duration-300">
+          <div className="flex items-center">
+            <span>Blogs</span>
+            <span className="ml-1">
+              <BookOpenCheck size={20} />
+            </span>
+          </div>
+        </li>
+        <li className="hover:text-gray-600 transition-all duration-300">
+          <div className="flex items-center">
+            <span>Profile</span>
+            <span className="ml-1">
+              <User size={20} />
+            </span>
+          </div>
+        </li>
+      </ul>
     </nav>
   );
 }
