c763d43fef7ab6e91280b0d145e7e28fd551dc6c
Afaaf Hariri <haririafaaf@gmail.com>
2025-03-21 17:22:46 +0530

    navbar props and isactive

--- next/src/components/nav-bar.jsx
+++ next/src/components/nav-bar.jsx
@@ -3,21 +3,45 @@ import { LayoutDashboard } from "lucide-react";
 import { NotebookPen } from "lucide-react";
 import { BookOpenCheck } from "lucide-react";
 
-function NavBar() {
+function NavBar({ currentPage = "dashboard" }) {
+  const isActive = (page) => currentPage === page;
   return (
-    <nav className="bg-bg-[var(--background)] h-screen w-16 p-6 fixed border-r border-[var(--border)]">
+    <nav className="bg-[var(--background)] h-screen w-16 p-6 fixed border-r border-[var(--border)]">
       <ul className="flex flex-col space-y-4 list-none font-semibold items-center">
-        <li className="hover:text-gray-600 transition-all duration-300">
+        <li
+          className={`transition-all duration-300 ${
+            isActive("dashboard")
+              ? "text-blue-500"
+              : "text-gray-600 hover:text-gray-800"
+          }`}
+        >
           <LayoutDashboard size={20} />
-          {/* <div className="flex items-center"> */}
         </li>
-        <li className="hover:text-gray-600 transition-all duration-300">
+        <li
+          className={`transition-all duration-300 ${
+            isActive("journals")
+              ? "text-blue-500"
+              : "text-gray-600 hover:text-gray-800"
+          }`}
+        >
           <NotebookPen size={20} />
         </li>
-        <li className="hover:text-gray-600 transition-all duration-300">
+        <li
+          className={`transition-all duration-300 ${
+            isActive("blogs")
+              ? "text-blue-500"
+              : "text-gray-600 hover:text-gray-800"
+          }`}
+        >
           <BookOpenCheck size={20} />
         </li>
-        <li className="hover:text-gray-600 transition-all duration-300">
+        <li
+          className={`transition-all duration-300 ${
+            isActive("profile")
+              ? "text-blue-500"
+              : "text-gray-600 hover:text-gray-800"
+          }`}
+        >
           <User size={20} />
         </li>
       </ul>
