8d76a10b66a238dab391955a9a81c8b01457c7fd
Afaaf Hariri <haririafaaf@gmail.com>
2025-03-21 17:17:07 +0530

    footer

--- next/src/components/footer.jsx
+++ next/src/components/footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-[var(--background)] text-[var(--foreground)] px-8 py-5 flex justify-end items-center border-t border-[var(--border)]">
+    <footer className="bg-[var(--background)] text-[var(--foreground)] px-8 py-5 flex justify-end items-center ">
       <p className="text-sm mr-2">
         made by <span className="font-bold">WE_IT_105</span>
       </p>
