1af2b2eb880020e0563fc4ffa49634ac42c4668d
Afaaf Hariri <haririafaaf@gmail.com>
2025-03-22 15:14:59 +0530

    button and dot pattern

--- next/src/app/page.jsx
+++ next/src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <WarpBackground padding={5}>
-      <div className="min-h-screen p-5 bg-gradient-to-br from-white via-blue-50 to-purple-50 rounded-lg">
+      <div className="min-h-screen py-5 px-10 bg-gradient-to-br from-purple-50 via-blue-50 to-purple-50 rounded-lg">
         <div className="flex flex-col justify-start items-center w-64 shrink-0">
           <p className="text-6xl font-bold bg-gradient-to-br from-purple-600 via-purple-500 to-purple-600 bg-clip-text text-transparent">
             mindcare
@@ -33,6 +33,14 @@ export default function Home() {
           <div className="flex flex-col justify-center items-center w-1/2 text-purple-500">
             <SmilePlus size={400} />
           </div>
+        </div>
+        <div className="mt-16 w-full flex flex-col space-y-5 justify-center items-start ml-15">
+          <button className="p-4 bg-transparent border border-purple-500 text-purple-500 font-semibold font-mono hover:text-white hover:bg-purple-500 transition-all duration-300">
+            HEY, I AM A NEW USER!
+          </button>
+          <button className="p-4 bg-transparent border border-purple-500 text-purple-500 font-semibold font-mono hover:text-white hover:bg-purple-500 transition-all duration-300">
+            I ALREADY HAVE AN ACCOUNT
+          </button>
         </div>
       </div>
     </WarpBackground>
