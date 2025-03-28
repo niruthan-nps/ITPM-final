d5f3cb7edc6f06e88863e6761aaa67e9a3c9174b
Afaaf Hariri <haririafaaf@gmail.com>
2025-03-28 19:37:39 +0530

    signup page

--- next/src/app/signup/page.jsx
+++ next/src/app/signup/page.jsx
@@ -1,49 +1,52 @@
-import { Smile, SmilePlus } from "lucide-react";
+import { SmilePlus } from "lucide-react";
+import { WarpBackground } from "@components/magicui/warp-background";
 function SignUp() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <form className="min-w-96 rounded-lg flex flex-col justify-center items-center bg-gradient-to-br from-purple-50 via-blue-50 to-purple-50">
-        <div className="flex w-full justify-center items-center mt-5">
-          <p className="text-3xl font-bold bg-gradient-to-br from-purple-600 via-purple-500 to-purple-600 bg-clip-text text-transparent">
-            mindcare
-          </p>
-          <SmilePlus size={35} className="ml-2 text-purple-500" />
-        </div>
-        <div className="flex w-full justify-start items-center px-3 mt-5">
-          <label className="text-purple-500 font-mono font-semibold">
-            First Name
-          </label>
-        </div>
-        <div className="flex w-full justify-start items-center px-3">
-          <input
-            type="text"
-            className="w-full p-2 bg-gray-100 border border-purple-200 focus:outline-none focus:border-purple-500"
-          />
-        </div>
-        <div className="flex w-full justify-start items-center px-3 mt-5">
-          <label className="text-purple-500 font-mono font-semibold">
-            Last Name
-          </label>
-        </div>
-        <div className="flex w-full justify-start items-center px-3">
-          <input
-            type="text"
-            className="w-full p-2 bg-gray-100 border border-purple-200 focus:outline-none focus:border-purple-500"
-          />
-        </div>
-        <div className="flex w-full justify-start items-center px-3 mt-5">
-          <label className="text-purple-500 font-mono font-semibold">
-            Email
-          </label>
-        </div>
-        <div className="flex w-full justify-start items-center px-3">
-          <input
-            type="email"
-            className="w-full p-2 bg-gray-100 border border-purple-200 focus:outline-none focus:border-purple-500"
-          />
-        </div>
-      </form>
-    </div>
+    <WarpBackground>
+      <div className="min-h-screen flex justify-center items-center">
+        <form className="min-w-96 rounded-lg flex flex-col justify-center items-center bg-gradient-to-br from-purple-50 via-blue-50 to-purple-50">
+          <div className="flex w-full justify-center items-center mt-5">
+            <p className="text-3xl font-bold bg-gradient-to-br from-purple-600 via-purple-500 to-purple-600 bg-clip-text text-transparent">
+              mindcare
+            </p>
+            <SmilePlus size={35} className="ml-2 text-purple-500" />
+          </div>
+          <div className="flex w-full justify-start items-center px-3 mt-5">
+            <label className="text-purple-500 font-mono font-semibold">
+              First Name
+            </label>
+          </div>
+          <div className="flex w-full justify-start items-center px-3">
+            <input
+              type="text"
+              className="w-full p-2 bg-gray-100 border border-purple-200 focus:outline-none focus:border-purple-500"
+            />
+          </div>
+          <div className="flex w-full justify-start items-center px-3 mt-5">
+            <label className="text-purple-500 font-mono font-semibold">
+              Last Name
+            </label>
+          </div>
+          <div className="flex w-full justify-start items-center px-3">
+            <input
+              type="text"
+              className="w-full p-2 bg-gray-100 border border-purple-200 focus:outline-none focus:border-purple-500"
+            />
+          </div>
+          <div className="flex w-full justify-start items-center px-3 mt-5">
+            <label className="text-purple-500 font-mono font-semibold">
+              Email
+            </label>
+          </div>
+          <div className="flex w-full justify-start items-center px-3">
+            <input
+              type="email"
+              className="w-full p-2 bg-gray-100 border border-purple-200 focus:outline-none focus:border-purple-500"
+            />
+          </div>
+        </form>
+      </div>
+    </WarpBackground>
   );
 }
 export default SignUp;
